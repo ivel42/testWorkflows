db2133317c358e400f7df72f43eec309db5dd9b2
andreas.lefevre <andreas.lefevre@gmx.de>
2023-08-04 22:34:45 +0200

    fix missing #

--- build_doc.ps1
+++ build_doc.ps1
@@ -6,7 +6,7 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 .\.venv/Scripts/Activate.ps1  -Prompt "venv"
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
-echo "generated with 'pip freeze' to make sure github action doesent update env when no update - hint from https://github.com/actions/setup-python" > requirements_frozen.txt
+echo "# generated with 'pip freeze' to make sure github action doesent update env when no update - hint from https://github.com/actions/setup-python" > requirements_frozen.txt
 
 pip freeze >> requirements_frozen.txt
 
