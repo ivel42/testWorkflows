bbd81230c3fc4fae5c22abf96a839118eaf0d2a5
andreas.lefevre <andreas.lefevre@gmx.de>
2023-08-04 21:26:33 +0200

    add python venv with sphinx

---
+++ build_doc.ps1
@@ -0,0 +1,12 @@
+# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+
+python -m venv .venv
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
+.\.venv/Scripts/Activate.ps1  -Prompt "venv"
+python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
+echo "generated with "pip freeze" to make sure github action doesent update env when no update" > requirements_frozen.txt
+
+pip freeze >> requirements_frozen.txt
+
+deactivate
